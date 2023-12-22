973dcd2fa9eeacda9193f9ffb9b9a43c069bf610
41023134 <41023134@gm.nfu.edu.tw>
2023-12-22 14:48:46 +0800

    add w15資料夾

---
+++ downloads/w15/w15_zmq.py
@@ -0,0 +1,26 @@
+# pip install pyzmq cbor keyboard
+from zmqRemoteApi import RemoteAPIClient
+from zmqRemoteApi_IPv6 import RemoteAPIClient
+
+
+
+
+client = RemoteAPIClient('localhost', 23000)
+#client = RemoteAPIClient('2001:288:6004:17:2023:cda:4:1', 23000)
+
+print('Program started')
+sim = client.getObject('sim')
+#sim.startSimulation()
+print('Simulation started')
+
+
+
+shape = sim.getObject('/Sphere')
+sensor = sim.getObject('/sensor')
+
+ 
+while(True):
+    r =sim.readProximitySensor(sensor)
+    if r[0]==1 :
+        sim.addForceAndTorque(shape,[1,0,0],[1,0,0])
+
